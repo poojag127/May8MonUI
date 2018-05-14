cd12f1deabb51744b113ba61162b9dfc4a5301b3
NishaSingh <nishasingh.ns1994@gmail.com>
2018-05-14 11:12:21 +0530

    Changes regarding css for configuration fieldset in health check monitor

---
+++ containers/health-check-data.ts
@@ -0,0 +1,15 @@
+export class HealthCheckMonData
+{
+ packet:number =5;
+ interval:number ;
+ proxyUrl:string;
+ userName:string;
+ pwd:string;
+ statusCode:string;
+ threadPool:number;
+ timeOut:number;
+ tierServerType:string;
+ tierName:string;
+ serverName:string;
+ 
+}
